4f9627cf7c697a9ed2f485e3639a3c13ca23d913
emedat24 <emedattitus24@gmail.com>
2023-09-12 05:28:28 -0700

    update

--- myAPI/blob/main/index.js
+++ myAPI/blob/main/index.js
@@ -3,27 +3,29 @@ const app = express();
 const port = 3000;
 
 app.get('/api', (req, res) => {
-	const slack_name = req.query.slack_name
-	const track = req.query.track;
+  const slackName = req.query.slack_name;
+  const track = req.query.track;
 
-	const date = new Date();
-	const current_day = date.toLocaleString('en-us', { weekday: 'long' });
-	const utc_time = date.toISOString();
+  const date = new Date();
+  const currentDay = date.toLocaleString('en-us', { weekday: 'long' });
+  const utcTime = date.toISOString();
 
-	const github_file_url = 'https://github.com/EMEDAT/NPM_stage_1/blob/bb692c41352f1dc6bb032ecbcb9fecf0d9e947b7/myAPI/blob/main/index.js';
-	const github_repo_url = 'https://github.com/EMEDAT/NPM_stage_1';
-	
-	res.json({
-		slack_name: "Emediong David Titus",
-		current_day: "Tuesday",
-		utc_time: "2023-08-21T15;04:05Z",
-		track:"Backend",
-		github_file_url: "https://github.com/EMEDAT/NPM_stage_1/blob/bb692c41352f1dc6bb032ecbcb9fecf0d9e947b7/myAPI/blob/main/index.js",
-		github_repo_url = "https://github.com/EMEDAT/NPM_stage_1",
-		status_code: 200
-	});
+  const githubFileUrl = 'https://github.com/username/repo/blob/main/file_name.ext';
+  const githubRepoUrl = 'https://github.com/username/repo';
+
+  const response = {
+    slackName: 'Emediong David Titus',
+    currentDay: 'Tuesday',
+    utcTime: '2023-08-21T15;04:05Z',
+    track: 'Backend',
+    githubFileUrl: 'https://github.com/EMEDAT/NPM_stage_1/blob/bb692c41352f1dc6bb032ecbcb9fecf0d9e947b7/myAPI/blob/main/index.js',
+    githubRepoUrl: 'https://github.com/EMEDAT/NPM_stage_1.git',
+    status: 200
+  };
+
+  res.json(response);
 });
 
 app.listen(port, () => {
-	console.log('Server running at http://localhost:${port}');
+  console.log(`Server running at http://localhost:${port}`);
 });
