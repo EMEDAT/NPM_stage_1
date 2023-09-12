7112208e5e5d0062baef53687cd1f01e53f2623b
emedat24 <emedattitus24@gmail.com>
2023-09-12 04:45:16 -0700

    update

--- myAPI/blob/main/index.js
+++ myAPI/blob/main/index.js
@@ -10,7 +10,7 @@ app.get('/api', (req, res) => {
 	const current_day = date.toLocaleString('en-us', { weekday: 'long' });
 	const utc_time = date.toISOString();
 
-	const github_file_url = 'https://github.com/username/repo/blob/main/index.js';
+	const github_file_url = 'https://github.com/EMEDAT/NPM_stage_1/blob/bb692c41352f1dc6bb032ecbcb9fecf0d9e947b7/myAPI/blob/main/index.js';
 	const github_repo_url = 'https://github.com/EMEDAT/NPM_stage_1';
 	
 	res.json({
@@ -18,7 +18,7 @@ app.get('/api', (req, res) => {
 		current_day: "Tuesday",
 		utc_time: "2023-08-21T15;04:05Z",
 		track:"Backend",
-		github_file_url: "https://github.com/username/repo/blob/main/index.js",
+		github_file_url: "https://github.com/EMEDAT/NPM_stage_1/blob/bb692c41352f1dc6bb032ecbcb9fecf0d9e947b7/myAPI/blob/main/index.js",
 		github_repo_url = "https://github.com/EMEDAT/NPM_stage_1",
 		status_code: 200
 	});
